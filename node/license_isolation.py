fa9bcdc73b833dea8cbde5c931fb7fa9c831f641
kxing28 <kcxing@student.ubc.ca>
2022-03-30 01:13:16 -0700

    Can detect the back of cars (rectangles) and crops images to show just the backs and the license plates. Images are still shifted, may need to work on warping images.

--- node/license_isolation.py
+++ node/license_isolation.py
@@ -9,13 +9,14 @@ from std_msgs.msg import String
 import time
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class plateFinder:
 	DEBUG = 1
 	def __init__(self):
 		self.bridge = CvBridge()
-		if self.DEBUG:
+		if self.DEBUG: #reads images from a folder
 			self.images = []
 			self.folder = "/home/fizzer/ros_ws/src/Machine-Learning/training_images"
 			self.outFolder = "/home/fizzer/ros_ws/src/Machine-Learning/output_images"
@@ -31,20 +32,71 @@ class plateFinder:
 				try:
 					biggestContour=max(contours, key=cv2.contourArea)
 					print(str(cv2.contourArea(biggestContour)) + " " + img[1])
-					outimg = cv2.drawContours(img[0], biggestContour, -1, (0,255,0), 3)
+					contourIndex = -1
+					color = (0,255,0)
+					thickness = 2
+					#we need square brackets because drawContours expects a list of ndarrays
+					# outimg = cv2.drawContours(img[0], [biggestContour], contourIndex, color,thickness)
+
+					#approximating and detecting quadrilaterals
+					approx = cv2.approxPolyDP(biggestContour, 0.009 * cv2.arcLength(biggestContour, True), True)
+					outimg = cv2.drawContours(img[0], [approx], contourIndex, color,thickness)
+					print(approx)
+					print(approx.shape)
+					if len(approx) == 4:
+						xi = approx[0][0][0]
+						xf = approx[2][0][0]
+						
+						yi = approx[0][0][1]
+						yf = approx[2][0][1]
+						yf_new = yf + int(0.3 * (yf-yi))
+						# outimg = img[0][yi:yf_new, xi:xf]
+						outimg = self.shiftPerspective(approx, outimg)
+
 					cv2.imwrite(self.outFolder + "/out_" + img[1], outimg)
 				except Exception,e:
 					print str(e)
-					outimg = cv2.drawContours(img[0], contours, -1, (0,255,0), 3)
+					contourIndex = -1
+					color = (0,255,0)
+					thickness = 2
+					outimg = cv2.drawContours(img[0], contours, contourIndex, color, thickness)
 					cv2.imwrite(self.outFolder + "/out_" + img[1], outimg)
 					print("No contours in image")
 					continue
-		else:
+		else: #typical analysis with photos from Gazebo
 			rospy.init_node('license_plate_analysis')
 			self.license_photo_pub = rospy.Publisher('R1/license_photo', Image, queue_size=1)
 			time.sleep(1)
 			self.image_sub = rospy.Subscriber('/R1/pi_camera/image_raw', Image, self.callback)
 			rospy.spin()
+
+	def shiftPerspective(self, approx, img):
+		#CURRENTLY WIP. DOESN'T SHIFT PROPERLY, AND CUTS OFF PLATE!!
+		#Computing perspectiveshift and warpperspective
+		#https://theailearner.com/tag/cv2-warpperspective/
+		#(x,y) pairs
+		TL = approx[0][0]
+		BL = approx[1][0]
+		TR = approx[3][0]
+		BR = approx[2][0]
+
+		top_width = np.sqrt((TL[0] - TR[0])**2 + (TL[1] - TR[1])**2)
+		bot_width = np.sqrt((BL[0] - BR[0])**2 + (BL[1] - BR[1])**2)
+		max_width = max(int(top_width), int(bot_width))
+
+		left_height = np.sqrt((TL[0] - BL[0])**2 + (TL[1] - BL[1])**2)
+		right_height = np.sqrt((TR[0] - BR[0])**2 + (TR[1] - BR[1])**2)
+		max_height = max(int(left_height), int(right_height))
+
+		input_pts = np.float32([TL, BL, BR, TR])
+		output_pts = np.float32([[0,0], 
+						[0,max_height - 1],
+						[max_height - 1, max_width - 1],
+						[max_width - 1, 0]])
+
+		M = cv2.getPerspectiveTransform(input_pts, output_pts)
+		newImg = cv2.warpPerspective(img,M,(max_width, max_height),flags=cv2.INTER_LINEAR)
+		return newImg
 
 	def callback(self, data):
 		photo = self.plateIsolation(data)
